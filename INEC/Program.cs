d623bd3bab9232acb5b04bd43ffc533f4f439be9
Support <Support@DESKTOP-7DS253L>
2022-08-10 10:12:21 +0100

    Add project files.

---
+++ INEC/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace INEC
+{
+    internal class INECAss
+    {
+        static void Main(string[] args)
+        {
+
+
+        
+                SortedSet<string> voters = new SortedSet<string>();
+
+                voters.Add("anu");               
+                voters.Add("seun");
+                voters.Add("anu");
+                voters.Add("seun");
+                voters.Add("peace");
+                voters.Add("esther");
+                voters.Add("anu");
+                voters.Add("tola");
+                voters.Add("grace");
+                voters.Add("deborah");
+                voters.Add("anu");
+                voters.Add("tosin");
+
+
+          
+
+
+
+                Console.WriteLine("enter voter's name");
+                string newName = Console.ReadLine();
+
+
+                if (voters.Contains(newName))     //condition for existing user and new user
+                {
+                    Console.WriteLine("Voter already exists");
+                }
+                else
+                {
+                    voters.Add(newName);
+                    Console.WriteLine("User successfully added");
+
+                }
+            }
+        }
+    }
+
+  
+    
