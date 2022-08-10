7e1e1d5620ef522f46d806051a528660e339c665
Tosin20 <69603959+Tosin20@users.noreply.github.com>
2022-08-10 11:05:18 +0100

    Add files via upload

---
+++ INEC/INEC/Program.cs
@@ -0,0 +1,60 @@
+﻿namespace INEC
+{
+    internal class INECAss
+    {
+        static void Main(string[] args)
+        {
+
+
+        
+                SortedSet<string> voters = new SortedSet<string>();
+
+                voters.Add("anu");               
+                voters.Add("seun");
+                voters.Add("anu");
+                voters.Add("seun");
+                voters.Add("peace");
+                voters.Add("esther");
+                voters.Add("anu");
+                voters.Add("tola");
+                voters.Add("grace");
+                voters.Add("deborah");
+                voters.Add("anu");
+                voters.Add("tosin");
+
+
+                
+
+            try                                        //incase user iputs capital letter
+            {
+
+                Console.WriteLine("enter voter's name");
+                string newName = Console.ReadLine().ToLower();
+
+                if (voters.Contains(newName))     //condition for existing user and new user
+                {
+                    Console.WriteLine("Voter already exists");
+                }
+                else
+                {
+                    voters.Add(newName);
+                    Console.WriteLine("User successfully added");
+                }
+            }
+
+
+
+            catch
+            (Exception e)
+            {
+                Console.Write("Re-try with small letters:");
+            }
+           
+
+        }
+     }
+ }
+   
+
+  
+    
